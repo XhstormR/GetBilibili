4107a1256706358dfcfca2907f3d6c4a4ea49a82
XhstormR <674080894@qq.com>
2016-09-20 22:50:41 +0800

    Signed-off-by: XhstormR <X>

--- src/GetBilibili.java
+++ src/GetBilibili.java
@@ -57,7 +57,7 @@ public class GetBilibili {
                 DecimalFormat timeFormat = new DecimalFormat("00");
                 int s = Video_Length / 1000;
                 System.out.println("Title: " + Video_Title);
-                System.out.println("Total Size: " + sizeFormat.format(Video_Size / (1024 * 1024.0)) + " MB\t" + "Video Time: " + timeFormat.format(s / 60) + ":" + timeFormat.format(s % 60) + " Mins\n");
+                System.out.println("Total Size: " + sizeFormat.format(Video_Size / (1024 * 1024.0)) + " MB\t" + "Total Time: " + timeFormat.format(s / 60) + ":" + timeFormat.format(s % 60) + " Mins\n");
                 Link.forEach(System.out::println);
                 break;
             case "-m":
