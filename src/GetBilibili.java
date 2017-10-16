60b2d34ae73cf1d042580e70371d9a4253c6cf23
XhstormR <10527522+XhstormR@users.noreply.github.com>
2017-10-16 18:39:26 +0800

    Signed-off-by: XhstormR <X>

--- src/GetBilibili.java
+++ src/GetBilibili.java
@@ -1,15 +1,18 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
 
 public class GetBilibili {
     private static final String UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.89 Safari/537.36";
@@ -17,13 +20,14 @@ public class GetBilibili {
     private static final String YamdiLink = "http://ww4.sinaimg.cn/large/a15b4afegw1f7vhnqa9soj203k03kwfe";
     private static final String FFmpegLink = "http://ww4.sinaimg.cn/large/a15b4afegw1f7vhtyv0wbj203k03k4qz";
     private static final String SevenZipLink = "http://blog.xhstormr.tk/uploads/bin/7zr.exe";
+    private static final String Appkey = "85eb6835b0a1034e";
+    private static final String Secretkey = "2ad42749773c441109bdc0191257a664";
     private static File Dir;
-    private static String Url;
     private static String Cid;
     private static List<String> Link;
     private static boolean Delete = true;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, NoSuchAlgorithmException {
         String path = GetBilibili.class.getProtectionDomain().getCodeSource().getLocation().getPath();
         Dir = new File(path.substring(0, path.lastIndexOf('/')), "GetBilibili");
         if (!Dir.exists()) {
@@ -36,8 +40,7 @@ public class GetBilibili {
             return;
         } else switch (args[0]) {
             case "-l":
-                Url = args[1];
-                getCID();
+                getCID(args[1]);
                 getLink();
                 Link.forEach(System.out::println);
                 break;
@@ -47,38 +50,70 @@ public class GetBilibili {
                 mergeFLV();
                 break;
             case "-d":
-                Url = args[1];
-                getCID();
+                getCID(args[1]);
                 getLink();
                 saveLink();
                 downLoad();
                 listFile();
                 mergeFLV();
                 break;
+            default:
+                System.out.println("Error!!!");
+                return;
         }
-
         System.out.println("\n" + "Done!!!");
     }
 
-    private static void getCID() throws IOException {
-        Document doc = Jsoup.connect(Url).userAgent(UserAgent).get();
-        Element player = doc.getElementsByClass("scontent").select("script").get(0);
-        String data = player.data();
-        Cid = data.substring(data.indexOf("cid") + 4, data.indexOf('&'));
+    private static void getCID(String url) throws IOException {
+        /*Jsoup 解析页面获得 Cid*/
+//        Document doc = Jsoup.connect(url).userAgent(UserAgent).get();
+//        Element player = doc.getElementsByClass("scontent").select("script").get(0);
+//        String data = player.data();
+//        Cid = data.substring(data.indexOf("cid") + 4, data.indexOf('&'));
+
+        /*原生解析页面获得 Cid*/
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new GZIPInputStream(new URL(url).openStream()), "utf-8"));
+//        Scanner scanner = new Scanner(bufferedReader);
+//        for (String s; scanner.hasNextLine(); ) {
+//            s = scanner.nextLine();
+//            if (s.contains("cid=")) {
+//                bufferedReader.close();
+//                Cid = s.substring(s.indexOf("cid=") + 4, s.indexOf('&'));
+//                break;
+//            }
+//        }
+//        bufferedReader.close();
+
+        /*原生解析页面获得 Cid*/
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new GZIPInputStream(new URL(url).openStream()), "utf-8"));
+        for (String s; (s = bufferedReader.readLine()) != null; ) {
+            if (s.contains("cid=")) {
+                bufferedReader.close();
+                Cid = s.substring(s.indexOf("cid=") + 4, s.indexOf('&'));
+                break;
+            }
+        }
+        bufferedReader.close();
     }
 
-    private static void getLink() throws IOException {
-        String url = "http://www.bilibilijj.com/DownLoad/Cid/" + Cid;
-        Document doc = Jsoup.connect(url).userAgent(UserAgent).get();
-        Link = new ArrayList<>();
-        if (doc.baseUri().contains("DownLoad")) {
-            Elements elements = doc.getElementsByClass("D").select("a[href]");
-            for (Element element : elements) {
-                Link.add(element.attr("href"));
-            }
-        } else if (doc.baseUri().contains("FreeDown")) {
-            Link.add(doc.getElementsByClass("putong").parents().get(0).attr("href"));
-        }
+    private static void getLink() throws IOException, NoSuchAlgorithmException {
+        /*通过 Jsoup 解析第三方 BiliBiliJJ 获得 Link*/
+//        String url = "http://www.bilibilijj.com/DownLoad/Cid/" + Cid;
+//        Document doc = Jsoup.connect(url).userAgent(UserAgent).get();
+//        Link = new ArrayList<>();
+//        if (doc.baseUri().contains("DownLoad")) {
+//            Elements elements = doc.getElementsByClass("D").select("a[href]");
+//            for (Element element : elements) {
+//                Link.add(element.attr("href"));
+//            }
+//        } else if (doc.baseUri().contains("FreeDown")) {
+//            Link.add(doc.getElementsByClass("putong").parents().get(0).attr("href"));
+//        }
+
+        /*通过 JSON 解析官方 BiliBili 获得 Link*/
+        StringBuilder SubLink = new StringBuilder().append("appkey=").append(Appkey).append("&cid=").append(Cid).append("&otype=json&quality=3&type=flv");
+        String Sign = Hash(SubLink + Secretkey, "MD5");
+        Link = Json("http://interface.bilibili.com/playurl?" + SubLink + "&sign=" + Sign);
     }
 
     private static void saveLink() throws IOException {
@@ -123,19 +158,7 @@ public class GetBilibili {
                     return Integer.valueOf(s1).compareTo(Integer.valueOf(s2));
                 }
             });
-
             for (File file : files) {
-//                String s1 = file.getName();
-//                if (s1.substring(s1.indexOf("-") + 1, s1.indexOf(".")).length() < 2) {
-//                    String s2 = new StringBuilder(s1).insert(s1.indexOf("-") + 1, '0').toString();
-//                    file.renameTo(new File(file.getParent(), s2));
-//                    bufferedWriter.write(new StringBuilder("file 'Download/'").insert(15, s2).toString());
-//                    bufferedWriter.newLine();
-//                    continue;
-//                }
-//                bufferedWriter.write(new StringBuilder("file 'Download/'").insert(15, s1).toString());
-//                bufferedWriter.newLine();
-
                 String name = file.getName();
                 bufferedWriter.write(new StringBuilder("file 'Download/'").insert(15, name).toString());
                 bufferedWriter.newLine();
@@ -148,6 +171,7 @@ public class GetBilibili {
     }
 
     private static void mergeFLV() throws IOException, InterruptedException {
+        /*方案一*/
 //        String s1 = "cmd.exe /C start /W D:/ffmpeg.exe -f concat -i D:/2.txt -c copy D:/12.flv";
 //        Process exec1 = Runtime.getRuntime().exec(s1);
 //        exec1.waitFor();
@@ -155,6 +179,7 @@ public class GetBilibili {
 //        Process exec2 = Runtime.getRuntime().exec(s2);
 //        exec2.waitFor();
 
+        /*方案二*/
         if (!new File(Dir, "ffmpeg.exe").exists()) {
             getEXE(FFmpegLink);
         }
@@ -222,5 +247,46 @@ public class GetBilibili {
             }
         }
         process.waitFor();
+        bufferedReader.close();
+    }
+
+    private static String Hash(String str, String algorithm) throws NoSuchAlgorithmException {
+        /*方案一*/
+//        MessageDigest instance = MessageDigest.getInstance(algorithm);
+//        return new BigInteger(1, instance.digest(str.getBytes())).toString(16);//10->16
+
+        /*方案二*/
+        MessageDigest instance = MessageDigest.getInstance(algorithm);
+        byte[] digest = instance.digest(str.getBytes());
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (byte b : digest) {
+            String s = Integer.toHexString(b & 0xff);
+            stringBuilder = s.length() < 2 ? stringBuilder.append('0').append(s) : stringBuilder.append(s);
+        }
+        return stringBuilder.toString();
+    }
+
+    private static List<String> Json(String link) throws IOException {
+        /*通过自建 Bean 对象解析*/
+//        List<String> Link = new ArrayList<>();
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL(link).openStream(), "utf-8"));
+//        Video video = new Gson().fromJson(bufferedReader, Video.class);
+//        for (Url url : video.getDurl()) {
+//            Link.add(url.getUrl());
+//        }
+//        bufferedReader.close();
+//        return Link;
+
+        /*通过自带 Bean 对象解析*/
+        List<String> Link = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL(link).openStream(), "utf-8"));
+        JsonObject jsonObject = new JsonParser().parse(bufferedReader).getAsJsonObject();
+        JsonArray jsonArray = jsonObject.getAsJsonArray("durl");
+        for (JsonElement durl : jsonArray) {
+            Link.add(durl.getAsJsonObject().get("url").getAsString());
+        }
+        bufferedReader.close();
+        return Link;
     }
 }
